2419f4d8d13a51062c007f5191d1c2858b9c6510
Sachin2903 <sachin.18bme1170@abes.ac.in>
2023-06-19 00:03:18 +0530

    added navbar

---
+++ src/components/pages/navbar/navbar.js
@@ -0,0 +1,11 @@
+import React,{Fragment} from "react";
+import styles from "../../../sass/navbarStyle/navbar.module.css"
+export function Navbar(){
+    return(
+        <Fragment>
+            <div>
+                
+            </div>
+        </Fragment>
+    )
+}
